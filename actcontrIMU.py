c4dfb6157140061f66e440ea4684962baf32c36a
Arenas <jm.arenas10@uniandes.edu.co>
2019-05-27 15:16:48 -0500

    lazo cerrado

--- actcontrIMU.py
+++ actcontrIMU.py
@@ -12,7 +12,7 @@ import os.path
 
 
 
-def calcularFase(y1,y2,dt):
+def calcularFase(y1,y2,dt,w):
 
     xcorr=correlate(y1,y2)
     nsamples=np.size(y1)
@@ -50,18 +50,19 @@ def main():
     stick.init()
 
     # Parse options
-    parser = common_argument_parser(desc=main.__doc__)
-    args = parser.parse_args()
+    #parser = common_argument_parser(desc=main.__doc__)
+    #args = parser.parse_args()
 
     # Connect to the serial port
-    sc = Connection(port=args.port,
-                                   baudrate=args.baudrate,
-                                   timeout=args.timeout,
-                                   rpi_gpio=args.rpi)
+    #sc = Connection(port=args.port,
+                                   #baudrate=args.baudrate,
+                                   #timeout=args.timeout,
+                                   #rpi_gpio=args.rpi)
 
-    dynamixel_id = args.dynamixel_id
+    sc = Connection(port="/dev/ttyUSB0", baudrate=1000000)    
+    dynamixel_id = 1
     
-    freq=1.1
+    freq=0.9
     w=2*np.pi*freq
     Am=45
 
@@ -70,29 +71,33 @@ def main():
         pygame.event.get()
         iniciar=stick.get_button(1)
 
+    sc.set_cw_angle_limit(dynamixel_id,-150,degrees=True)
+    sc.set_ccw_angle_limit(dynamixel_id,150,degrees=True) 
+
     sc.goto(dynamixel_id, 0, speed=512, degrees=True)
+    time.sleep(1)
     start=time.time()
     offseto=0
     tant=time.time()-start
     pressed=False
-    Ndatos=80
-    datosroll=np.zeros([Ndatos/2])
-    datospmotor=np.zeros([Ndatos/2])
+    Ndatos=120
+    datosroll=np.zeros([int(Ndatos/2)])
+    datospmotor=np.zeros([int(Ndatos/2)])
 
     #contador para la ventana de tiempo
     i=0
 
-    dt=0.05
+    dt=0.01
 
     fase=np.pi/2
-    K=1
+    K=0.5
     K2=0.5
     #Amplitud deseada del roll
-    Ades=20
+    Ades=15
     Amax=Ades
     while(not pressed):
         
-        A=Am+int(K2(Ades-Amax))
+        A=Am+int(K2*(Ades-Amax))
 
 
         error=np.pi/2-fase
@@ -103,7 +108,7 @@ def main():
         arg=tiempo*w
         offsetn=round(stick.get_axis(0),2)
         der=A*(offsetn-offseto)/(tiempo-tant)
-        vel=np.abs(int((60*A*w*np.cos(w*tiempo+error)+60*der)*1023/(114*360)))
+        vel=int(np.abs((60*A*w*np.cos(w*tiempo+K*error)-60*der)*1023/(114*360)))
 
         if vel > 1023:
             vel=1023
@@ -112,34 +117,38 @@ def main():
             arg-=2*np.pi
 
         if arg <= np.pi/2 or (arg>=3*np.pi/2):
-            objetivo=A+int(A*offsetn)
+            objetivo=A-int(A*offsetn)
             sc.goto(dynamixel_id, objetivo, speed=vel, degrees=True)
 
         else:
-            objetivo=-A+int(A*offsetn)
+            objetivo=-A-int(A*offsetn)
             sc.goto(dynamixel_id, objetivo, speed=vel, degrees=True)
 
         #print(objetivo," ",vel," ",int(60*der*1023/(114*360)))clu
         pmotor=sc.get_present_position(dynamixel_id,degrees=True)
 
         #Daots de la IMU
+        leyo=False
         if imu.IMURead():
         # x, y, z = imu.getFusionData()
         # print("%f %f %f" % (x,y,z))
             data = imu.getIMUData()
             fusionPose = data["fusionPose"]
-            roll=math.degrees(fusionPose[0])
+            roll=np.degrees(fusionPose[1])
+         
+            leyo=True
         
         tant=time.time()-start
         offseto=offsetn
 
-        if i%2==0:
-            datosroll[i/2]=roll
-            datospmotor[i/2]=pmotor
+        if i%2==0 and leyo:
+            datosroll[int(i/2)]=roll
+            datospmotor[int(i/2)]=pmotor
 
         if i==Ndatos-1:
-            fase=calcularFase(datospmotor,datosroll,dt)
+            fase=calcularFase(datospmotor,datosroll,dt,w)
             Amax=int(np.amax(datosroll))
+            #print(Amax,fase)
 
         i+=1
         if i >= Ndatos:
@@ -149,7 +158,7 @@ def main():
         #time.sleep(poll_interval*1.0/1000.0)
 
     # Close the serial connection
-    serial_connection.close()
+    sc.close()
 
     while(True):
         pygame.event.get()
